dd198103f5a17d66d089af75d52aab532c5d5dbf
nicolyjjang <nicolyjjang@gmail.com>
2026-05-09 10:43:53 -0300

    feat: Adiciona lógica de vendas (controller, routes, services), adiciona venda.json.

--- mercadinho.js
+++ mercadinho.js
@@ -4,6 +4,9 @@ const port = 8000;
 
 app.use(express.json());
 
+const rotasVenda = require('./routes/routes_venda');
+app.use('/vendas', rotasVenda);
+
 app.use("/", (req, res) => {
   res.send("Bem-vindo ao Mercadinho São Miguel!");
 });
