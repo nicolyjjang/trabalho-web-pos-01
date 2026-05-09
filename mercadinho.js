3913f177bac70d94f9e7468d5243daf257fe345f
nicolyjjang <nicolyjjang@gmail.com>
2026-05-09 11:16:23 -0300

    feat: Adiciona lógica do backend de entrada (controller, routes, services) e entrada.json.

--- mercadinho.js
+++ mercadinho.js
@@ -7,6 +7,9 @@ app.use(express.json());
 const rotasVenda = require('./routes/routes_venda');
 app.use('/vendas', rotasVenda);
 
+const rotasEntrada = require('./routes/routes_entrada');
+app.use('/entradas', rotasEntrada);
+
 app.use("/", (req, res) => {
   res.send("Bem-vindo ao Mercadinho São Miguel!");
 });
