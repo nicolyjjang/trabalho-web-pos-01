9b1cdd454286a68833c3d6ead048e8519b23c959
Anderson Mendes <mendesandersonms@gmail.com>
2026-05-09 10:37:45 -0300

    adicionando rotas no mercadinho.js

--- mercadinho.js
+++ mercadinho.js
@@ -4,6 +4,12 @@ const port = 8000;
 
 app.use(express.json());
 
+const rotaPessoa = require("./routes/routes_pessoa");
+const rotaCampanha = require("./routes/campanha");
+
+app.use("/pessoa", rotaPessoa);
+app.use("/campanha", rotaCampanha);
+
 app.use("/", (req, res) => {
   res.send("Bem-vindo ao Mercadinho São Miguel!");
 });
